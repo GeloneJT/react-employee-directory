2add6b74a998cebb1e9141dd41d67e8364636d6c
Jelani Thomas <jelani13@icloud.com>
2021-04-13 07:05:37 -0400

    Header component added

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <h1>Employee Directory</h1>
+        <p>Input Name In Search Field To Search For An Employee</p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
